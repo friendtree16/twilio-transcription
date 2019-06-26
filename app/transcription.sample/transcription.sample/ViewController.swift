8bc7bc5b47afe03229a377e6fcdb91d6a30b8b26
葛 智紀 <tomoki.katsu@kddi-web.com>
2019-06-26 14:27:41 +0900

    appを追加

---
+++ app/transcription.sample/transcription.sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  transcription.sample
+//
+//  Created by 葛 智紀 on 2019/06/26.
+//  Copyright © 2019 葛 智紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app/transcription.sample/transcription.sampleTests/transcription_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  transcription_sampleTests.swift
+//  transcription.sampleTests
+//
+//  Created by 葛 智紀 on 2019/06/26.
+//  Copyright © 2019 葛 智紀. All rights reserved.
+//
+
+import XCTest
+@testable import transcription_sample
+
+class transcription_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
